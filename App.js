b20a2931b25e2d14298fd6b60500c48c99c398ad
samuel joseph (samstickkz) <39702515+samstickkz@users.noreply.github.com>
2023-06-11 22:41:28 +0100

    it is well

--- App.js
+++ App.js
@@ -1,20 +1,88 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  SafeAreaView,
+  Image,
+} from "react-native";
 
-export default function App() {
+const LoginScreen = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    // Perform login logic here
+    console.log("Username:", username);
+    console.log("Password:", password);
+  };
+
+  const navigateToHome = () => {
+    // Navigate to the Home screen
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.body}>
+        <Image style={styles.onlogo} source={require("./assets/Cards.png")} />
+        <View style={styles.h1view}>
+          <Text style={styles.h1t}>Seamless trading</Text>
+        </View>
+        <Text style={styles.h2t}>
+          Buy, sell and exchange cryptocurrencies.{" "}
+        </Text>
+
+        <TouchableOpacity style={styles.btnON} onPress={navigateToHome}>
+          <Text style={styles.getbtn}>Login</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "black",
+  },
+  body: {
+    padding: 16,
+  },
+
+  onlogo: {
+    width: 377,
+    height: 431,
+  },
+  h1t: {
+    marginTop: 40,
+    color: "white",
+    fontSize: "34",
+    fontWeight: "bold",
+  },
+  h2t: {
+    color: "#A7A7CC",
+    fontSize: 22,
+    marginTop: 12,
+  },
+  h1view: {
+    width: 300,
+    color: "red",
+  },
+  btnON: {
+    height: 54,
+    backgroundColor: "#9D62D9",
+    borderRadius: 5,
+    marginTop: 61,
+    alignItems: "center",
+  },
+  getbtn: {
+    marginTop: 15,
+    fontSize: 17,
+    color: "white",
+    fontWeight: "bold"
   },
 });
+
+export default LoginScreen;
